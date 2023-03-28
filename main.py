58965f5a839a4cab7d940fd8a0a9048e143abfc4
YSW2 <jrgyber@gmail.com>
2023-03-28 16:54:42 +0900

    2023-03-28 16:54

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ with open("configuration.json", "r") as config:
 intents = discord.Intents.all()
 # The bot
 bot = commands.Bot(command_prefix=prefix, intents=intents)
-
+openai_bot = openai_api.openai_bot()
 
 @bot.event
 async def on_ready():
@@ -26,27 +26,34 @@ async def on_ready():
 	print(discord.__version__)
 	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming, name =f"{bot.command_prefix}"))
 
+@bot.command()
+async def clear(ctx):
+	await openai_bot.messages_clear()
+	await ctx.send("청소 완료")
+	
 @bot.event
 async def on_message(message):
-	if message.author.bot:
+	if message.author.bot or message.content[1:] == "clear":
+		await bot.process_commands(message)
 		return
-	
+
 	content = message.content
 	prompts = []
-	'''if content.startswith(prefix):
-		result = await chatgpt.usegpt(content[1:])
-		await message.channel.send(result)'''
+	tasks = []
+	name = []
 	if content.startswith(prefix):
-		tasks = []
-		name = []
-		nickname = message.author.nick if message.author.nick else message.author.name
-		prompts.append([nickname, content])
+		prompts.append([message.author, content[1:]])
 		for nick, prompt in prompts:
 			name.append(nick)
-			tasks.append(asyncio.ensure_future(openai_api.usegpt(prompt)))
+			tasks.append(asyncio.ensure_future(openai_bot.usegpt(prompt)))
 		responses = await asyncio.gather(*tasks)
 
-		for i in range(len(responses)):
-			await message.channel.send(f"{name[i]}/ {responses[i]}")
-	
+		for i, response in enumerate(responses):
+			index = 0
+			while index < len(response):
+				await message.channel.send(f"{name[i].mention}{response[index: min(index+2000, len(response))]}")
+				index += 2000
+	tasks.clear()
+	name.clear()
+
 bot.run(token)
--- openai_api.py
+++ openai_api.py
@@ -3,25 +3,26 @@ import os
 
 openai.api_key = os.getenv('openai_API_KEY')
 
-with open("system_content.txt", 'r') as f:
-    sys_order = ' '.join(f.readlines())
+class openai_bot:
+    def __init__(self):
+        with open("system_content.txt", 'r') as f:
+            sys_order = ' '.join(f.readlines())
+        self.messages = [{"role": "system", "content": sys_order}]
+        self.token_num = 0
 
-messages = [{"role": "system",
-             "content": sys_order}]
+    async def usegpt(self, question):
+        # 질문과 대답 저장
+        self.messages.append({"role": "user", "content": question})
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=self.messages
+        )
+        print(completion)
+        answer = completion['choices'][0]['message']['content']
+        self.messages.append({"role": "assistant", "content": answer})
+        return answer
 
-async def usegpt(question):
-
-    print(f"{question}")
-
-    # 질문과 대답 저장
-
-    messages.append({"role": "user", "content": question})
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-    )
-    print(completion)
-    answer = completion['choices'][0]['message']['content']
-    messages.append({"role": "assistant", "content": answer})
-
-    return answer
+    async def messages_clear(self):
+        with open("system_content.txt", 'r') as f:
+            sys_order = ' '.join(f.readlines())
+        self.messages = [{"role": "system", "content": sys_order}]
