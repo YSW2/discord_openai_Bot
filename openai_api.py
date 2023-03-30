aa6747b101e4e74486e3204efcb36b0b60d53f27
YSW2 <jrgyber@gmail.com>
2023-03-30 14:07:52 +0900

    2023-03-30 14:07

--- openai_api.py
+++ openai_api.py
@@ -2,6 +2,8 @@ import openai
 import os
 
 openai.api_key = os.getenv('openai_API_KEY')
+if openai.api_key is None:
+    print("Not Available API_KEY")
 
 class openai_bot:
     def __init__(self):
@@ -11,13 +13,9 @@ class openai_bot:
         self.token_num = 0
 
     async def usegpt(self, question):
-        # 질문과 대답 저장
         self.messages.append({"role": "user", "content": question})
         completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=self.messages
-        )
-        print(completion)
+            model="gpt-3.5-turbo", messages=self.messages)
         answer = completion['choices'][0]['message']['content']
         self.messages.append({"role": "assistant", "content": answer})
         return answer
